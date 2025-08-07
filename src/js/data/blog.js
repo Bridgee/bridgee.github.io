da45e1745d7e0ac98f24c04d420573f1716431c3
Zhouqiao Zhao <bridgeaffiliation@gmail.com>
2025-08-07 09:54:04 -0400

    Fix file organization and component refactoring

--- src/js/data/blog.js
+++ src/js/data/blog.js
@@ -16,6 +16,7 @@ export const blogData = {
   links: {
     googleScholar: "https://scholar.google.com/citations?user=Y1s8cw0AAAAJ&hl",
     linkedin: "https://www.linkedin.com/in/zhouqiao-zhao-60560a56/",
+    researchGate: "https://www.researchgate.net/profile/Zhouqiao-Zhao",
     labWebsite: "https://ctl.mit.edu/about/bio/zhouqiao-bridge-zhao",
     cv: "/doc/Zhouqiao_Zhao_Resume_2025_Jul.pdf",
     soundcloud: "https://soundcloud.com/zhouqiao-zhao",
